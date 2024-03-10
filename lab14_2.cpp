b77474be44c60686549755a1663b96859481962c
Teetat Yodbun <teetatjj1@gmail.com>
2024-03-10 15:48:23 +0700

    oifnw

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -19,3 +19,39 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void inputMatrix(double X[][N]){
+    for(int i=0 ; i<N ; i++ ){
+        cout << "Row "<< i+1 << ": " ;
+        for(int j=0 ; j<N ; j++ ){
+            cin >> X[i][j]; 
+        }
+
+    }
+}
+
+void findLocalMax(const double X[][N], bool Y[][N]){
+    for(int i=0 ; i<N ; i++ ){
+        for(int j=0 ; j<N ; j++ ){
+            Y[i][j] = 0;
+            }
+        }
+    for(int i=1 ; i<N-1 ; i++ ){
+        for(int j=1 ; j<N-1 ; j++ ){
+            if( X[i][j] >= X[i-1][j] && X[i][j] >= X[i+1][j] && X[i][j] >= X[i][j-1] && X[i][j] >= X[i][j+1] ){
+                Y[i][j] = true ;
+                }
+            else Y[i][j] = false ;
+            }
+        
+        }
+}
+
+void showMatrix(const bool Y[][N]){
+    for(int i=0 ; i<N ; i++ ){
+        for(int j=0 ; j<N ; j++ ){
+            cout << Y[i][j] << " " ; 
+            }
+        cout << "\n" ;
+        }
+}
